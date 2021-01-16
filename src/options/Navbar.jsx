d118bad177af14e83923b7ef3b2b56677471f98c
Aryan Kalia <kalia.aryan2013@gmail.com>
2021-01-16 16:34:26 -0500

    Logo fixes

--- src/options/Navbar.jsx
+++ src/options/Navbar.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import logo from './Logo_Full.png'
+import Image from 'react-bootstrap/Image'
 
 class NavBar extends Component {
     state = {  }
     render() { 
         return (
         <Navbar id="nav" collapseOnSelect expand="lg" sticky="top">
-            <Navbar.Brand className="ml-2" id="nav-title" className="raleway letters-wide">JOBBERS</Navbar.Brand>
+            <Navbar.Brand href="#" className="ml-2" id="nav-title" className="raleway letters-wide"><Image height="50vh" src={logo}></Image></Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="ml-auto">
