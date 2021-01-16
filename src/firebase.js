266375136c54091839b237db9719fe79fa9121ca
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2021-01-16 13:22:26 -0500

    Firestore Commit

---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+import app from "firebase/app"
+import 'firebase/database'
+
+var firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    databaseURL: "https://jobsearchautomater-default-rtdb.firebaseio.com",
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_APP_ID
+  };
+
+
+class Firebase
+{
+    constructor()
+    {
+        app.initializeApp(firebaseConfig);
+    }
+}
+
+
+
+export default Firebase;
+
+
--- src/options/Options.js
+++ src/options/Options.js
@@ -1,25 +1,45 @@
 import logo from '../logo.svg';
+import React, { Component } from 'react';
 import './Options.css';
+import firebase from 'firebase/app'
+import 'firebase/database'
+import databaseHelper from './databaseFunctions';
 
-function Options() {
-  return (
+var dbhelper = new databaseHelper;
+
+class Options extends Component {
+  sendToFirebase()
+  {
+    //dbhelper.logFirebaseDb();
+      //const databaseRef = firebase.database().ref('users');
+      
+      //console.log("hi");
+      var data = {
+        name : "asd",
+        phoneNumber: 43
+      }
+      
+      dbhelper.sendToFirestore(data);
+      //bhelper.retrieveInfo();
+  }
+
+  /****Startup Callback on react****/
+  componentDidMount()
+  {
+    console.log(firebase);
+    
+  }
+
+  render()
+  {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/options/Options.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" onClick={this.sendToFirebase}>idk</button>
     </div>
-  );
+    )
+  }
+
+  
 }
 
 export default Options;
