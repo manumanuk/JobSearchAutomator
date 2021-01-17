e5c52e29382d53c91fbdb95738e2413ea99dfad1
upadhyaydhruv <dmupadhyay12@gmail.com>
2021-01-16 22:39:05 -0500

    Removed a bunch of unnecessary files

--- parser/backend_parser.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-
-FILE_TO_UPLOAD_PATH = Path(
-    "/home/goodmanmcgee/JobSearchAutomator/parser/Resume_V1.pdf")
-
-with open(FILE_TO_UPLOAD_PATH, "rb") as doc_file:
-    print(type(doc_file))
