7257ead3b0065c184a02ff4b2c3934ac80ca8ca3
soumavm <soumavm@gmail.com>
2021-01-17 00:35:56 -0700

    Switch statement Hell

--- src/contentScript/index.js
+++ src/contentScript/index.js
@@ -1,8 +1,8 @@
+/*global chrome*/
 import Firebase from '../firebase';
 import databaseHelper from '../databaseFunctions';
 
 var dbhelper = new databaseHelper();
-
 var currState = document.getElementsByTagName('h2')[0]
 var currStateString = "";
 var prevStateString ="";
@@ -10,10 +10,7 @@ var recorded = false;
 var wordBank = ["first name", "last name", "full name", "name", "city", "country", 
     "province", "postal", "state", "address", "phone number", "phone extension", "university", 
     "degree", "program", "gpa", "grade", "from", "to", "skill", "linkedin", "github"]
-
-var numberOfStatusUpdates = 0;
-
-
+    var numberOfStatusUpdates = 0;
 function stateUpdate(){
   
     currState = document.getElementsByTagName('h2')[0]
@@ -32,16 +29,11 @@ function stateUpdate(){
           getPageHTML()
           console.log("I delayed")
         }, 2000);
-      /***CALL FUNCTIONS TO DO STUFF***/
-      //testDB();
-      //console.log(currStateString);
-      //console.log(dbhelper.retrieveInfo());
-
     }
   }
   }
 
-getPageHTML();
+
 function getPageHTML() {
   {
     var label = document.getElementsByTagName('label')
@@ -57,6 +49,54 @@ function getPageHTML() {
           var ID = label[i].htmlFor
           var form = document.getElementById(ID)
           //change the value to the database value of whatever is being stored
+          
+          switch(j){
+            case 0:
+              form.value = data.name.firstName
+              break;
+            case 1:
+              form.value = data.name.lastName
+              break;
+            case 2:
+            case 3:
+              form.value = data.name.firstName + data.name.lastName
+              break;
+            case 4:
+              form.value = data.location.city
+              break;
+            case 5:
+              form.value = data.location.country
+              break;
+            case 6:
+            case 8:
+              form.value = data.location.state
+              break;
+            case 7:
+              form.value = data.location.postalCode
+              break;
+            case 9:
+              form.value = data.location.address
+              break;
+            case 10:
+              form.value = data.phoneNumber
+              break;
+            case 11:
+              form.value = "+1"
+              break;
+            case 12:
+              form.value = data.education.organization
+              break;
+            case 13:
+              form.value = data.education.degree
+            case 14:
+              form.value = data.education.program
+              break;
+            case 15:
+            case 16:
+              form.value = data.education.grade
+              break;
+            }
+
           form.value = "Aryan"
         }
       }
@@ -64,8 +104,16 @@ function getPageHTML() {
   }
 }
 
-document.addEventListener('DOMNodeInserted', stateUpdate);
 
+  let info = dbhelper.retrieveInfo()
+  let keys = Object.keys(info);
+  let data = keys[0]
+
+
+
+
+getPageHTML();
+document.addEventListener('DOMNodeInserted', stateUpdate);
 
 /*****FIREBASE SETUP*****/
 var firebaseClass = new Firebase();
@@ -80,3 +128,11 @@ function testDB()
 
       dbhelper.sendToFirestore(test);
 }
+
+chrome.runtime.onMessage.addListener(
+  function(message, sender, sendResponse){
+    console.log(message)
+  }
+)
+
+
